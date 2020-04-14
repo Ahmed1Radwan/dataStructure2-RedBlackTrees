f41d26e3e2d8bc4493ab5c99101ce295a2b5cb17
Ahmed <ahmedradwan111111@gmail.com>
2020-04-14 14:53:15 +0200

    last

--- dataStructureLab2/src/eg/edu/alexu/csd/filestructure/redblacktree/MAIN.java
+++ dataStructureLab2/src/eg/edu/alexu/csd/filestructure/redblacktree/MAIN.java
@@ -10,11 +10,7 @@ public class MAIN {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//TreeMap<Integer, String> t = new TreeMap();
-		ITreeMap<Integer,String> treemap = new TreeMap();
-		
-        treemap.put(5, "soso5");
-       System.out.println(treemap.lastEntry());
+	
 		
 	}
 	
